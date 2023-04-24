5fae215541cf122f73d3450dafe9b83fdf9038b6
muehlefeldt <max.muehlefeldt@tu-berlin.de>
2023-04-25 00:46:36 +0200

    Logging.

--- python/basic_rl_env/hyperparameter_search.py
+++ python/basic_rl_env/hyperparameter_search.py
@@ -1,6 +1,9 @@
 import yaml
 from pathlib import Path
 import os
+import logging
+
+
 
 # Paths: Config files and unity env.
 path_to_working_dir = "python/basic_rl_env"
@@ -12,6 +15,15 @@ path_to_unity_env = "./build"
 if (os.getcwd() != Path(path_to_working_dir).absolute()):
     os.chdir(Path(path_to_working_dir).absolute())
 
+def get_run_id() -> int:
+    dir_content = os.listdir(Path("./results/").absolute())
+    numbers = []
+    for entry in dir_content:
+        numbers.append(int(entry.split("_")[0]))
+    return max(numbers) + 1
+
+logging.basicConfig(filename=f"{get_run_id()}_search.log", level=logging.INFO)
+
 # Open the base config file.
 with open(Path(path_to_config_file).absolute(), mode="r") as config_file:
     config = yaml.safe_load(config_file)
@@ -19,26 +31,19 @@ with open(Path(path_to_config_file).absolute(), mode="r") as config_file:
 # Select parameter to modify in base config.
 para = {
     "beta": [
-        1e-4,
-        5e-4,
+        #1e-4,
+        #5e-4,
         1e-3,
-        5e-3,
-        1e-2
+        #5e-3,
+        #1e-2
     ],
     "lambd": [
-        0.85,
-        0.9,
+        #0.85,
+        #0.9,
         0.925,
-        0.95
+        #0.95
     ]
 }
-
-def get_run_id() -> int:
-    dir_content = os.listdir(Path("./results/").absolute())
-    numbers = []
-    for entry in dir_content:
-        numbers.append(int(entry.split("_")[0]))
-    return max(numbers) + 1
 
 for beta_value in para["beta"]:
     # Reload the base config. Modify selected parameter.
@@ -50,6 +55,10 @@ for beta_value in para["beta"]:
 
         # Id number of the run. As shown in tensorboard. Needed to ensure traceability.
         run_id_num = get_run_id()
+
+        logging.info(f"[{get_run_id}] New run started with id {get_run_id}.")
+        logging.info(f"[{get_run_id}] beta = {beta_value}.")
+        logging.info(f"[{get_run_id}] lambd = {lambd_value}.")
 
         # Reload the base config. Modify selected parameter.
         #tmp_config = config
@@ -69,4 +78,7 @@ for beta_value in para["beta"]:
             --force"
         )
 
-        print(f"{run_id_num} beta={beta_value} and lambd={lambd_value}, code {return_code}.")
+        if return_code != 0:
+            logging.warning(f"[{get_run_id}] error code.")
+
+        logging.info(f"[{get_run_id}] return code = {return_code}.")
