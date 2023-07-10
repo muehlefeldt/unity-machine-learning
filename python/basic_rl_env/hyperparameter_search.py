f7966ae3ee36569221187417579851dd52b76915
Max Mühlefeldt <max.muehlefeldt@tu-berlin.de>
2023-07-10 18:31:29 +0200

    Calculation of possible calculations moved in to one single dict.

--- python/basic_rl_env/hyperparameter_search.py
+++ python/basic_rl_env/hyperparameter_search.py
@@ -25,47 +25,65 @@ def get_run_id() -> int:
     return max(numbers) + 1
 
 
-def get_parameter_combinations(parameters) -> tuple[list, list]:
+def get_dynamic_parameters(base_config: list[dict]) -> list:
+    key_values = []
+    for section in base_config:
+        parameters = section[list(section.keys())[0]]
+        for key in parameters:
+            # Check if value for key is a list. If so, store key value pair.
+            if isinstance(parameters[key], list):
+                new = []
+                for entry in parameters[key]:
+                    new.append({list(section.keys())[0]: {key: entry}})
+                key_values.append(new)
+
+    return key_values
+
+
+def get_parameter_combinations(parameters: list[list]) -> list[tuple]:
     """
     Get the parameter combinations.
     """
-    key_values = []
-    for key in parameters:
-        # Check if value for key is a list. If so, store key value pair.
-        if isinstance(parameters[key], list):
-            new = []
-            for entry in parameters[key]:
-                new.append({key: entry})
-            key_values.append(new)
-
-    combinations = list(itertools.product(*key_values))
+    # key_values = []
+    # for key in parameters:
+    # Check if value for key is a list. If so, store key value pair.
+    #    if isinstance(parameters[key], list):
+    #        new = []
+    #        for entry in parameters[key]:
+    #            new.append({"network": {key: entry}})
+    #        key_values.append(new)
+    # test = *parameters
+    combinations = list(itertools.product(*parameters))
     if production:
-        logging.info("Found %i dynamic values.", len(key_values))
+        logging.info("Found %i value combinations.", len(combinations))
     return combinations
 
 
-def update_parameter(base: dict, option: tuple, id_num: int) -> dict:
+def update_parameters_with_option(base: dict, option: tuple, id_num: int):
     """
     Update key value pairs in temporary dict using the key values list and the current option.
     The run id is only used for logging purposes.
     """
-    # Work on copy of the base config values only.
-    tmp = dict.copy(base)
+    work_dict = base["behaviors"]["RollerAgent"]
 
     # Dynamic key value pairs are updated in the tmp dict.
     for entry in option:
         # Get the key for the entry in the option.
-        key = list(entry.keys())[0]
+        entry_key = list(entry.keys())[0]
+        para_key = list(entry[entry_key].keys())[0]
 
         # Value for the given key.
-        value = entry[key]
+        value = entry[entry_key][para_key]
 
-        # Update the temporary dict.
-        tmp[key] = value
+        if entry_key == "memory":
+            work_dict["network_settings"][entry_key][para_key] = value
+        else:
+            # Update the temporary dict.
+            work_dict[entry_key][para_key] = value
 
         if production:
-            logging.info(f"[{id_num}] {key} = {value}.")
-    return tmp
+            logging.info(f"[{id_num}] {para_key} = {value}.")
+    return
 
 
 # def get_number_of_runs(list_of_key_values: list[dict]) -> int:
@@ -164,31 +182,36 @@ if production:
 if production and message_for_log is not None:
     logging.info("Note: %s", message_for_log)
 
-hyperparamters = config["behaviors"]["RollerAgent"]["hyperparameters"]
-network = config["behaviors"]["RollerAgent"]["network_settings"]
+hyperparamters = {"hyperparameters": config["behaviors"]["RollerAgent"]["hyperparameters"]}
+network = {"network_settings": config["behaviors"]["RollerAgent"]["network_settings"]}
+
+# base_config = config["behaviors"]["RollerAgent"]
 
 # In case memory is configured in yaml file:
 # Handle memory options seperate from network settings.
 memory_comb = [()]
-memory = None
-if "memory" in network:
-    memory = config["behaviors"]["RollerAgent"]["network_settings"]["memory"]
-    network.pop("memory")
-    memory_comb = get_parameter_combinations(memory)
+memory = {"memory": {}}
+if "memory" in network["network_settings"]:
+    memory = {"memory": config["behaviors"]["RollerAgent"]["network_settings"]["memory"]}
+    # network["network_settings"].pop("memory")
+    # memory_comb = get_parameter_combinations(memory)
 
-hyper_comb = get_parameter_combinations(hyperparamters)
-network_comb = get_parameter_combinations(network)
+# hyper_comb = get_parameter_combinations(hyperparamters)
+# network_comb = get_parameter_combinations(network)
+
+dynamic_parameters = get_dynamic_parameters([hyperparamters, network, memory])
+combinations = get_parameter_combinations(dynamic_parameters)
 
 # Prepare summary.
-if generate_summary:
-    headings = []
-    for section in [hyper_comb[0], network_comb[0], memory_comb[0]]:
-        for entry in section:
-            headings.append(list(entry.keys())[0])
-    summary_dict = {}
+# if generate_summary:
+#    headings = []
+#    for section in [hyper_comb[0], network_comb[0], memory_comb[0]]:
+#        for entry in section:
+#            headings.append(list(entry.keys())[0])
+#    summary_dict = {}
 
 # Get the number of runs the current config is goint to produce.
-num_count = len(memory_comb) * len(network_comb) * len(hyper_comb)
+num_count = len(combinations)
 if production:
     logging.info("%i runs are going to be started.", num_count)
 
@@ -197,99 +220,96 @@ run_durations = []
 run_counter = 0
 
 # Combine all possible options for possible runs.
-for hyperparameter_option in hyper_comb:
-    for network_option in network_comb:
-        for memory_option in memory_comb:
-            run_counter += 1
+for option in combinations:
+    run_counter += 1
 
-            # Id number of the run. As shown in tensorboard. Needed to ensure traceability.
-            run_id = get_run_id()
-            path_to_temp_config_file = f"./configs/{run_id}.yaml"
+    # Id number of the run. As shown in tensorboard. Needed to ensure traceability.
+    run_id = get_run_id()
+    path_to_temp_config_file = f"./configs/{run_id}.yaml"
 
-            if production:
-                logging.info(f"[{run_id}] New run started with id {run_id}.")
+    if production:
+        logging.info(f"[{run_id}] New run started with id {run_id}.")
 
-            # Get copy of the base config as loaded.
-            tmp_config = dict.copy(config)
+    # Get copy of the base config as loaded.
+    tmp_config = dict.copy(config)
 
-            tmp_hyper = update_parameter(hyperparamters, hyperparameter_option, run_id)
-            tmp_config["behaviors"]["RollerAgent"]["hyperparameters"] = tmp_hyper
+    update_parameters_with_option(tmp_config, option, run_id)
+    # tmp_hyper = update_parameter(hyperparamters, hyperparameter_option, run_id)
+    # tmp_config["behaviors"]["RollerAgent"]["hyperparameters"] = tmp_hyper
 
-            tmp_network = update_parameter(network, network_option, run_id)
-            tmp_config["behaviors"]["RollerAgent"]["network_settings"] = tmp_network
+    # tmp_network = update_parameter(network, network_option, run_id)
+    # tmp_config["behaviors"]["RollerAgent"]["network_settings"] = tmp_network
 
-            # Memory might not be specified in the yaml file.
-            if memory is not None:
-                tmp_memory = update_parameter(memory, memory_option, run_id)
-                tmp_config["behaviors"]["RollerAgent"]["network_settings"]["memory"] = tmp_memory
+    # Memory might not be specified in the yaml file.
+    # if memory is not None:
+    #    tmp_memory = update_parameter(memory, memory_option, run_id)
+    #    tmp_config["behaviors"]["RollerAgent"]["network_settings"]["memory"] = tmp_memory
 
-            # Save modified config as yaml file.
-            if production:
-                with open(
-                    Path(path_to_temp_config_file).absolute(), mode="w", encoding="utf8"
-                ) as new_file:
-                    yaml.dump(tmp_config, new_file)
+    # Save modified config as yaml file.
+    if production:
+        with open(Path(path_to_temp_config_file).absolute(), mode="w", encoding="utf8") as new_file:
+            yaml.dump(tmp_config, new_file)
 
-            # Execute ml-agents using a compiled environment.
-            # Bypass if in test mode.
-            return_code = 0
-            start_time = time.time()
+    # Execute ml-agents using a compiled environment.
+    # Bypass if in test mode.
+    return_code = 0
+    start_time = time.time()
+    run_name = f"{run_id}_basicenv_ppo_auto"
+    if production:
+        if use_build_env:
             run_name = f"{run_id}_basicenv_ppo_auto"
-            if production:
-                if use_build_env:
-                    run_name = f"{run_id}_basicenv_ppo_auto"
-                    # Start ml-algents using build version of unity.
-                    return_code = os.system(
-                        f"mlagents-learn \
-                        {Path(path_to_temp_config_file).absolute()} \
-                        --env={Path(path_to_unity_env).absolute()} \
-                        --run-id={run_name}\
-                        --num-envs={num_env} \
-                        --torch-device cpu \
-                        --force"
-                    )
-                else:
-                    # Start ml-agents in the unity editor. Requires user interaction.
-                    run_name = f"{run_id}_basicenv_ppo_manual"
-                    return_code = os.system(
-                        f"mlagents-learn \
-                        {Path(path_to_temp_config_file).absolute()} \
-                        --run-id={run_name} \
-                        --num-envs={num_env} \
-                        --no-graphics \
-                        --torch-device cpu \
-                        --force"
-                    )
+            # Start ml-algents using build version of unity.
+            return_code = os.system(
+                f"mlagents-learn \
+                {Path(path_to_temp_config_file).absolute()} \
+                --env={Path(path_to_unity_env).absolute()} \
+                --run-id={run_name}\
+                --num-envs={num_env} \
+                --torch-device cpu \
+                --force"
+            )
+        else:
+            # Start ml-agents in the unity editor. Requires user interaction.
+            run_name = f"{run_id}_basicenv_ppo_manual"
+            return_code = os.system(
+                f"mlagents-learn \
+                {Path(path_to_temp_config_file).absolute()} \
+                --run-id={run_name} \
+                --num-envs={num_env} \
+                --no-graphics \
+                --torch-device cpu \
+                --force"
+            )
 
-            end_time = time.time()
-            if production:
-                logging.info(f"[{run_id}] return code = {return_code}.")
+    end_time = time.time()
+    if production:
+        logging.info(f"[{run_id}] return code = {return_code}.")
 
-            # Logging and error code handling.
-            if return_code != 0:
-                if production:
-                    logging.warning(f"[{run_id}] error code.")
-            else:
-                run_durations.append(end_time - start_time)
-                duration = (num_count - run_counter) * mean(run_durations)
+    # Logging and error code handling.
+    if return_code != 0:
+        if production:
+            logging.warning(f"[{run_id}] error code.")
+    else:
+        run_durations.append(end_time - start_time)
+        duration = (num_count - run_counter) * mean(run_durations)
 
-                if production:
-                    logging.info(f"[{run_id}] Duration: {int(run_durations[-1])} sec.")
-                    logging.info(f"[{run_id}] Avg. duration: {int(mean(run_durations))} sec.")
-                    logging.info(
-                        f"[{run_id}] Expected end time of all runs: {time.strftime('%d %b %Y %H:%M:%S', time.localtime(time.time() + duration))}."
-                    )
+        if production:
+            logging.info(f"[{run_id}] Duration: {int(run_durations[-1])} sec.")
+            logging.info(f"[{run_id}] Avg. duration: {int(mean(run_durations))} sec.")
+            logging.info(
+                f"[{run_id}] Expected end time of all runs: {time.strftime('%d %b %Y %H:%M:%S', time.localtime(time.time() + duration))}."
+            )
 
-                if generate_summary:
-                    summary_dict[run_id] = {}
-                    for entry in [
-                        *hyperparameter_option,
-                        *network_option,
-                        *memory_option,
-                    ]:
-                        summary_dict[run_id].update(entry)
+        # if generate_summary:
+        #    summary_dict[run_id] = {}
+        #    for entry in [
+        #        *hyperparameter_option,
+        #        *network_option,
+        #        *memory_option,
+        #    ]:
+        #        summary_dict[run_id].update(entry)
 
-                    summary_dict[run_id]["last_cumulative_reward"] = get_mean_reward(run_name)
+        #    summary_dict[run_id]["last_cumulative_reward"] = get_mean_reward(run_name)
 
 # Create a summary file to provide an overview of used parameters and resulting rewards.
 if generate_summary:
